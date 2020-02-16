7da749cdeaa15e36b1bc410e93b594d6ceefda51
VictorHugo0139 <60369276+VictorHugo0139@users.noreply.github.com>
2020-02-16 17:30:10 -0600

    Proyecto web
    Cambié la carpeta del proyecto por esta.

---
+++ AgroItesz/build/web/ViewClientes.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : Clientes
+    Created on : 16-feb-2020, 16:43:28
+    Author     : Victor
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Edición de clientes</title>
+    </head>
+    <body>
+        <h1>Clientes</h1>
+    </body>
+</html>
---
+++ AgroItesz/src/java/Conexion/Conexion.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Conexion;
+
+import java.sql.Statement;
+
+public class Conexion {
+
+    private java.sql.Connection connection = null;
+    private final String url = "jdbc:sqlserver://";
+    private final String serverName = "DESKTOP-1I12CES";
+    private final String portNumber = "1433";
+    private final String databaseName = "ERP2020";
+    private final String userName = "sa";
+    private final String password = "Chitoge39";
+    private final String statement = "select * from clientes;";
+    // Informs the driver to use server a side-cursor,
+    // which permits more than one active statement
+    // on a connection.
+    //private final String selectMethod = "Direct";
+
+    // Constructor
+    public Conexion() {
+    }
+
+    private String getConnectionUrl() {
+        return url + serverName + ":" + portNumber + ";databaseName=" + databaseName + ";";//+"selectMethod="+ selectMethod + ";";
+    }
+
+    private java.sql.Connection getConnection() {
+        try {
+            //Class.forName("com.microsoft.jdbc.sqlserver.SQLServerDriver");
+            connection = java.sql.DriverManager.getConnection(getConnectionUrl(),
+                    userName, password);
+            if (connection != null) {
+                System.out.println("Connection Successful!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error Trace in getConnection() : " + e.getMessage());
+        }
+        return connection;
+    }
+
+    /*
+  * Display the driver properties, database details
+     */
+    public void displayDbProperties() {
+        java.sql.DatabaseMetaData dm = null;
+        java.sql.ResultSet result = null;
+        try {
+            connection = this.getConnection();
+            if (connection != null) {
+                dm = connection.getMetaData();
+                System.out.println("Driver Information");
+                System.out.println("\tDriver Name: " + dm.getDriverName());
+                System.out
+                        .println("\tDriver Version: " + dm.getDriverVersion());
+                System.out.println("\nDatabase Information ");
+                System.out.println("\tDatabase Name: " + dm.getDatabaseProductName());
+                System.out.println("\tDatabase Version: " + dm.getDatabaseProductVersion());
+
+                Statement select = connection.createStatement();
+                result = select.executeQuery(statement);
+
+                while (result.next()) {
+                    System.out.println("Nombre: " + result.getString(1) + "\n");
+                    System.out.println("Apellido: " + result.getString(2) + "\n");
+                    System.out.println("Dni: " + result.getString(3) + "\n");
+                }
+                result.close();
+                result = null;
+                closeConnection();
+            } else {
+                System.out.println("Error: No active Connection");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        dm = null;
+    }
+
+    private void closeConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            connection = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Conexion myDbTest = new Conexion();
+        myDbTest.displayDbProperties();
+    }
+}
