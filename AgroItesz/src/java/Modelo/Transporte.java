ff0780187351e3b3f3e16f62779974b528ba833f
AldairOrtiz9850 <60710532+AldairOrtiz9850@users.noreply.github.com>
2020-03-11 21:04:03 -0600

    llenado Transporte
    se empezo a trabajar en la clase transporte

---
+++ AgroItesz/src/java/Modelo/Transporte.java
@@ -0,0 +1,16 @@
+
+package Modelo;
+
+public class Transporte {
+    private int idTransporte;
+    private String Placas;
+    private String Marca;
+    private String Modelo;
+    private int Año;
+    private int Capacidad;
+    private char estado;
+    
+    public Transporte(){
+        
+    }
+}
